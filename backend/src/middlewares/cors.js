9d6ff388a469c4cc97c223fd52e6f9c5dafcc4fc
Matheus Todao <todaomatheus@hotmail.com>
2021-12-17 22:20:38 -0300

    env

--- backend/src/middlewares/cors.js
+++ backend/src/middlewares/cors.js
@@ -1,5 +1,5 @@
 module.exports = (request, response, next) => {
-  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  response.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
   response.setHeader('Access-Control-Max-Age', '8600');
   next();
 };
